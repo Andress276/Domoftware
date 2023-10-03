60ed41a5d272c05c33bb5ecde08255715ff4b65d
perrovolador <danialejocruz@gmail.com>
2023-10-03 15:05:11 -0500

    actalizacion navegabilidad

--- app/src/main/java/com/example/domoftware/MainActivity.kt
+++ app/src/main/java/com/example/domoftware/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.domoftware
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // En el método onCreate de MainActivity
+        val registerButton = findViewById<Button>(R.id.button2)
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/domoftware/RegisterActivity.kt
+++ app/src/main/java/com/example/domoftware/RegisterActivity.kt
@@ -1,11 +1,21 @@
 package com.example.domoftware
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        // En el método onCreate de RegisterActivity
+        val loginButton = findViewById<Button>(R.id.buttonLogin)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
